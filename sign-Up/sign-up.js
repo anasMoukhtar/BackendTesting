30c5162f9ab8576f50820a565af201dd24ba923f
anasMoukhtar <137163749+anasMoukhtar@users.noreply.github.com>
2025-01-01 03:39:21 +0200

    Add files via upload

---
+++ sign-Up/sign-up.js
@@ -0,0 +1,9 @@
+
+const User = {
+    name : document.getElementById('name') ,
+    email : document.getElementById('email'),
+    password : document.getElementById('password') ,
+    confirmPassword : document.getElementById('confirmPassword') ,
+}
+function submitBtn(){
+}
