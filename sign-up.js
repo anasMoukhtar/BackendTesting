7186a1ce40624e511c13da8cf24658fcbc0008c2
anasMoukhtar <137163749+anasMoukhtar@users.noreply.github.com>
2025-01-01 05:33:26 +0200

    Delete sign-up.js

--- sign-up.js
+++
@@ -1,9 +0,0 @@
-
-const User = {
-    name : document.getElementById('name') ,
-    email : document.getElementById('email'),
-    password : document.getElementById('password') ,
-    confirmPassword : document.getElementById('confirmPassword') ,
-}
-function submitBtn(){
-}
